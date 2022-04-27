0598b6b173b5c7acc213c06814861758e121c528
Aisha Kimpson <akimp313@gmail.com>
2022-04-27 00:45:04 -0400

    create table/employee_role

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     deptname VARCHAR(30) NOT NULL
 );
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT, 
+);

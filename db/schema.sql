d437392f28a0b69b86b3b5e66a7a77881d550db3
Aisha Kimpson <akimp313@gmail.com>
2022-04-27 00:35:15 -0400

    create table/department

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
 DROP DATABASE IF EXISTS employeetracker_db;
 CREATE DATABASE employeetracker_db;
 
+USE employeetracker_db;
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    deptname VARCHAR(30) NOT NULL
+);

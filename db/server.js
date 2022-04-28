221e7b67db6d506838527db13a5e7be3c9cbb1b4
Aisha Kimpson <akimp313@gmail.com>
2022-04-28 17:59:10 -0400

    updated server.js added function runSearch

--- db/server.js
+++ db/server.js
@@ -4,8 +4,24 @@ const inquirer = require("inquirer");
 const connection = mysql.createConnection({
     host: "localhost",
     port:3306,
-    user: "root",
+    user: "akimpson",
     password: "@$$2312oK",
     database: "employee_tracker_db"
 });
 
+function runSearch() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Quit",
+        ]
+    })
